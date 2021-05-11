6dc59850a516877b7ac0c13ee4ab846cc8c65f0a
andreiabds <andreia.bdsodre@gmail.com>
2021-05-10 23:43:01 -0700

    initial reserve doses

--- COVID19_vaccine.py
+++ COVID19_vaccine.py
@@ -30,5 +30,32 @@ class COVID19Vaccine:
             print("ERROR: Could not add new doses. Pymssql error: " + db_err.args[1])
         return None
 
-    def ReserveDoeses():
-        return None
+    def ReserveDoeses(cursor, VaccineAppointmentId):
+        try:
+            sqltext = ''' SELECT va.vaccineid AS vaccineid, va.firstshot as firstshot, v.shotsnecessary AS shotsnecessary, v.inventory AS inventory, v.reserved AS reserved
+            FROM VaccineAppointment as va, Vaccines as v, CareGiverSchedule as cgs, AppointmentStatusCodes as statuscode 
+            WHERE va.vaccineid=v.vaccine_id AND cgs.VaccineAppoinmentid=va.VaccineAppointmentId AND statuscode.StatusCodeId=cgs.SlotStatus
+            AND VaccineAppointmentId=%s AND statuscode.StatusCode=''Open'' 
+            '''
+
+
+            cursor.execute(sqltext, (VaccineAppointmentId))
+            row = cursor.fetchone()
+            current_inventory = row['inventory']
+            shots_necessary = row['shotsnecessary']
+            is_first_shot = row['firstshot']
+            current_reserved = row['reserved'] 
+
+            if shots_necessary < current_inventory: 
+                raise Exception( "Not enough vaccines. Please try a different vaccine or another vaccine!")
+            else:
+                sqltext = "UPDATE Vaccines SET inventory=%s, reserved=%s WHERE vaccineid=%s"
+                if is_first_shot==1: 
+                    cursor.execute(sqltext, ((current_inventory - shots_ncessary), (current_reserved + shots_necessary), (vaccineid)))
+                    cursor.connection.commit()
+                    new_reserved = current_reserved + shots_necessary
+                    print("Successfully reserved %s doses from vaccineid %s. Current reserved doses: %s", shots_necessary, vaccineid, new_reserved)
+        except pymssql.Error as db_err:
+            print("ERROR: Could not reserve new doses. Pymssql error: " + db_err.args[1])
+        return None 
+
